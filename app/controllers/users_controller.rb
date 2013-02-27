6560ef4ec8bc4aa999d16c6e86b3f4ae973492fb
David <djcorp10@smumn.edu>
2013-02-27 14:41:47 -0600

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,59 +5,55 @@ class UsersController < ApplicationController
   
   def show
    @user = User.find(params[:id])
-  end
+   @microposts = @user.microposts.paginate(page: params[:page])
+ end
 
-  def new
+ def new
    @user = User.new
-  end
+ end
 
-  def create
+ def create
    @user = User.new(params[:user])
    if @user.save
-      sign_in @user
-   flash[:success] = "Welcome to the Sample App!"
-   redirect_to @user
-   else
+    sign_in @user
+    flash[:success] = "Welcome to the Sample App!"
+    redirect_to @user
+  else
    render 'new'
-   end
+ end
+end
+
+def edit
+end
+
+def update
+  if @user.update_attributes(params[:user])
+    flash[:success] = "Profile updated"
+    sign_in @user
+    redirect_to @user
+  else
+    render 'edit'
   end
+end
 
-  def edit
-  end
+def index
+  @users = User.paginate(page: params[:page])
+end
 
-  def update
-    if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updated"
-      sign_in @user
-      redirect_to @user
-    else
-      render 'edit'
-    end
-  end
+def destroy
+  User.find(params[:id]).destroy
+  flash[:success] = "User destroyed."
+  redirect_to users_path
+end
 
-  def index
-    @users = User.paginate(page: params[:page])
-  end
+private
 
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_path
-  end
+def correct_user
+  @user = User.find(params[:id])
+  redirect_to(root_path) unless current_user?(@user)
+end
 
-  private
-  
-    def signed_in_user
-      store_location
-      redirect_to signin_path, notice: "Please sign in." unless signed_in?
-    end
-
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
-    end
-
-    def admin_user
-      redirect_to(root_path) unless current_user.admin?
-    end
+def admin_user
+  redirect_to(root_path) unless current_user.admin?
+end
 end
